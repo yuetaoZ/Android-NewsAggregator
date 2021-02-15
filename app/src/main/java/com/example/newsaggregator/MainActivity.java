9bd020f74438efdfe31a9d3377346b8e1868631e
yuetaoZ <yuetao.zhu.062984@gmail.com>
2021-02-15 17:39:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/newsaggregator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.newsaggregator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //My API Key: 65d5db554f32406b97783439ae2d53cc
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
