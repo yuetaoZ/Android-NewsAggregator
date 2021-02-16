098cb6c69c2d61a4b804ce09b41bd59179d57930
yuetaoZ <yuetao.zhu.062984@gmail.com>
2021-02-16 11:31:22 -0600

    Added fields and background picture for MainActivity.

--- app/src/main/java/com/example/newsaggregator/MainActivity.java
+++ app/src/main/java/com/example/newsaggregator/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.newsaggregator;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     //My API Key: 65d5db554f32406b97783439ae2d53cc
+    private static final String TAG = "MainActivity";
+    private final ArrayList<String> newsSourceDisplayed = new ArrayList<>();
+    private final HashMap<String, ArrayList<String>> TCL2sourceData = new HashMap<>();   // TCL for Topics, Countries, and Languages
+    private Menu opt_menu;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private List<Fragment> fragments;
+    private MyPageAdapter pageAdapter;
+    private ViewPager pager;
+    private String currentSource;
+    public static int screenWidth, screenHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private class MyPageAdapter extends FragmentPagerAdapter {
+        private long baseId = 0;
+
+
+        MyPageAdapter(FragmentManager fm) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        public int getItemPosition(@NonNull Object object) {
+            return POSITION_NONE;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // give an ID different from position when position has been changed
+            return baseId + position;
+        }
+
+        /**
+         * Notify that the position of a fragment has been changed.
+         * Create a new ID for each position to force recreation of the fragment
+         * @param n number of items which have been changed
+         */
+        void notifyChangeInPosition(int n) {
+            // shift the ID returned by getItemId outside the range of all previous fragments
+            baseId += getCount() + n;
+        }
+
     }
 }
